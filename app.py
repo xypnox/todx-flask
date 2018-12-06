eed85d12bc6fe1752af867598784602c45c07eb3
xypnox <xypnox@gmail.com>
2018-12-06 21:54:01 +0530

    Modiefied some migrations

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
+from models import User
 
 @app.route('/')
 def hello():
